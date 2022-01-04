f949275ad89e4ecf63a95813c927869e58c11649
waldowred5 <dwaldow@me.com>
2022-01-04 12:51:02 +1100

    fix: bottom mobile nav hidden behind native browser bottom nav

--- src/components/atoms/AnimationCanvasHero/styles.js
+++ src/components/atoms/AnimationCanvasHero/styles.js
@@ -5,7 +5,7 @@ const StyledCanvas = styled.div`
   // Display
   display: flex;
   
-  height: calc(100vh - 231px);
+  height: calc(100vh - 167px);
   // TO:DO Do this properly
   
   ${media[SCREEN_SIZE.TABLET]} {
